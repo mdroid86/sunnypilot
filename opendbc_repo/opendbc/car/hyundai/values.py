e5a262a7364122536aabe6dc9f8e06c5c5b9d32d
mdroid86 <olssona722@pm.me>
2025-08-07 18:15:03 -0400

    values.py
    testing steering.

--- opendbc_repo/opendbc/car/hyundai/values.py
+++ opendbc_repo/opendbc/car/hyundai/values.py
@@ -29,7 +29,7 @@ class CarControllerParams:
       self.STEER_DRIVER_ALLOWANCE = 350
       self.STEER_DRIVER_MULTIPLIER = 2
       self.STEER_THRESHOLD = 300
-      self.STEER_DELTA_UP = 3
+      self.STEER_DELTA_UP = 4
       self.STEER_DELTA_DOWN = 3
 
     # To determine the limit for your car, find the maximum value that the stock LKAS will request.
